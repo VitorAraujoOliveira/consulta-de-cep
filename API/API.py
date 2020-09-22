d344cfef79234ad2e2601ba65c22843648b3467c
VitorAraujoOliveira <vitor.araujo934@hotmail.com>
2020-09-22 17:19:22 -0300

    0.0.4

--- API/API.py
+++ API/API.py
@@ -1,6 +1,8 @@
 import flask
 import json
 
+from scrapper import *
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -9,5 +11,18 @@ app.config["DEBUG"] = True
 def home():
 
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+
+
+@app.route('/cep/correios/', methods=['POST'])
+def cep_correios(cep):
+
+    return verifica_cep_correios(cep)
+
+
+
+
+
 
 app.run()
--- API/scrapper.py
+++ API/scrapper.py
@@ -2,28 +2,31 @@ import pycep_correios
 import time
 #from API import *
 import pandas as pd
+import warnings
+import numpy as np
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-
+import json
 
 def verifica_cep_correios(cep):
     
     k = len(cep)-1
 
     while cep != '00000000':
-
+        try:
+            endereco = pycep_correios.get_address_from_cep(cep)
+            return json.dumps(endereco)
+        except:
+            endereco = ("CEP inválido: " + cep)
+            pass
+       
 
         cep = cep[:k]
         
         cep = cep + '0'*(8-k)
         k = k -1
 
-        try:
-            endereco = pycep_correios.get_address_from_cep(cep)
-            return endereco
-        except:
-            endereco = ("CEP inválido: " + cep)
-            pass
-       
+
 
         print(endereco)
         #return endereco
@@ -39,23 +42,45 @@ def verifica_cep_local(cep):
 
     while cep != '00000000':
 
+        df = pd.read_csv("..\\Base de Ceps\\ceps.csv",error_bad_lines=False, 
+        delimiter=';',low_memory=False,  dtype={'cep': object, 'complemento1': object, 'complemento2': object, 'info': object})
+
+
+        #print(df[df['cep']==cep]['cep'].values[0])
+        if(df[df['cep']==cep]['cep'].values):
+
+            cep = (df[df['cep']==cep]['cep'].values[0])
+            logra = (df[df['cep']==cep]['logradouro'].values[0])
+            cidade = (df[df['cep']==cep]['cidade/estado'].values[0].split('/')[0])
+            estado = (df[df['cep']==cep]['cidade/estado'].values[0].split('/')[1])
+            bairro = (df[df['cep']==cep]['bairro'].values[0])
+
+            if str(df[df['cep']==cep]['info'].values[0]) != 'nan':
+                complemento = (str(df[df['cep']==cep]['info'].values[0]) + ' ' 
+                        + str(df[df['cep']==cep]['complemento1'].values[0]) + ' ' 
+                        + str(df[df['cep']==cep]['complemento2'].values[0])),
+            else:
+                complemento = ''
+
+            return ({'bairro': bairro, 
+                    'cep': cep, 
+                    'cidade': cidade, 
+                    'logradouro': logra, 
+                    'uf': estado, 
+                    'complemento': complemento})
 
         cep = cep[:k]
         
         cep = cep + '0'*(8-k)
         k = k -1
-
-        try:
-            endereco = pycep_correios.get_address_from_cep(cep)
-            return endereco
-        except:
-            endereco = ("CEP inválido: " + cep)
-            pass
-       
-
-        print(endereco)
-        #return endereco
     
     return ("Cep não encontrado")
 
 
+
+def salva_log(resultado_consulta):
+    return '' 
+
+
+def busca_log():
+    return ''
