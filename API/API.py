bc6e605151c850de88fb30ac781746dcfcd5412b
VitorAraujoOliveira <vitor.araujo934@hotmail.com>
2020-09-23 14:53:45 -0300

    0.0.5

--- API/API.py
+++ API/API.py
@@ -1,25 +1,65 @@
 import flask
 import json
+from flask import request
 
 from scrapper import *
+from flask import Flask
+from flask_cors import CORS
+
+from flask_cors import CORS, cross_origin
+import logging
+
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/', methods=['GET'])
+
 def home():
 
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Consulta CEP</h1><p>Consulte a documentação para as rotas!</p>"
+
+
+@app.route('/cep/logs/', methods=['GET'])
+@cross_origin()
+def logs():
+    return busca_log()
 
 
 
 
 @app.route('/cep/correios/', methods=['POST'])
-def cep_correios(cep):
-
+@cross_origin()
+def cep_correios():
+    cep = request.json['cep']
+    print(cep)
     return verifica_cep_correios(cep)
 
+
+
+@app.route('/cep/local/', methods=['POST'])
+@cross_origin() 
+def cep_local():
+    cep = request.json['cep']
+    return verifica_cep_local(cep)
 
 
 
--- API/scrapper.py
+++ API/scrapper.py
@@ -5,22 +5,34 @@ import pandas as pd
 import warnings
 import numpy as np
 warnings.simplefilter(action='ignore', category=FutureWarning)
+from datetime import datetime
+from datetime import date
 
 import json
 
+
+
 def verifica_cep_correios(cep):
-    
+    cep_inicial = cep
     k = len(cep)-1
 
     while cep != '00000000':
         try:
             endereco = pycep_correios.get_address_from_cep(cep)
-            return json.dumps(endereco)
-        except:
-            endereco = ("CEP inválido: " + cep)
+
+            salva_log('Correios' + ';' + 'CEP encontrado' + ';' 
+                + cep_inicial + ';' + endereco['cep'] + ';' 
+                + endereco['cidade'] + ';' + endereco['uf'] + ';' 
+                + endereco['logradouro'] + ';' + endereco['bairro'])
+            
+            return (endereco)
+
+        except Exception as err:
+            endereco = ("CEP inválido: " + cep + str(err))
             pass
        
 
+        
         cep = cep[:k]
         
         cep = cep + '0'*(8-k)
@@ -28,28 +40,29 @@ def verifica_cep_correios(cep):
 
 
 
-        print(endereco)
+
         #return endereco
     
+    salva_log('Correios' + ';' + "Cep não encontrado")
     return ("Cep não encontrado")
 
 
 
 
 def verifica_cep_local(cep):
-    
+    cep_inicial = cep
     k = len(cep)-1
 
     while cep != '00000000':
 
-        df = pd.read_csv("..\\Base de Ceps\\ceps.csv",error_bad_lines=False, 
+        df = pd.read_csv("..\\Base de Ceps\\ceps.csv",error_bad_lines=False, encoding = "ISO-8859-1",
         delimiter=';',low_memory=False,  dtype={'cep': object, 'complemento1': object, 'complemento2': object, 'info': object})
 
 
         #print(df[df['cep']==cep]['cep'].values[0])
         if(df[df['cep']==cep]['cep'].values):
 
-            cep = (df[df['cep']==cep]['cep'].values[0])
+            ceps = (df[df['cep']==cep]['cep'].values[0])
             logra = (df[df['cep']==cep]['logradouro'].values[0])
             cidade = (df[df['cep']==cep]['cidade/estado'].values[0].split('/')[0])
             estado = (df[df['cep']==cep]['cidade/estado'].values[0].split('/')[1])
@@ -62,25 +75,41 @@ def verifica_cep_local(cep):
             else:
                 complemento = ''
 
-            return ({'bairro': bairro, 
-                    'cep': cep, 
+
+            retorno = ({'bairro': bairro, 
+                    'cep': ceps, 
                     'cidade': cidade, 
                     'logradouro': logra, 
                     'uf': estado, 
                     'complemento': complemento})
 
+
+            
+            salva_log('Local' + ';' + 'CEP encontrado' + ';' + cep_inicial + ';' + ceps + ';' 
+                    + cidade + ';' + estado + ';' + logra + ';' + bairro)
+            return retorno
+
         cep = cep[:k]
         
         cep = cep + '0'*(8-k)
         k = k -1
-    
+
+    salva_log('Local' + ';' + "Cep não encontrado" + ';' + ';' + ';' + ';' + ';' + ';' )
     return ("Cep não encontrado")
 
 
 
 def salva_log(resultado_consulta):
-    return '' 
+    now = datetime.now()
+    data = now.strftime("%d/%m/%Y %H:%M:%S")
+    with open("..\\LOGs\\logs.csv", "a") as myfile:
+        myfile.write(resultado_consulta + ';' + data + '\n')
+
 
 
 def busca_log():
-    return ''
+    df = pd.read_csv("..\\LOGs\\logs.csv",error_bad_lines=False, encoding = "ISO-8859-1",
+        delimiter=';',low_memory=False,  dtype={'cep_pesquisado': object, 'cep_retornado': object})
+
+    return df.to_json()
+
